5258c1fb03c6b45329eed019443d92b9ed8d2276
Thomas J. Laffoon <tjl@thomasjlaffoon.com>
2014-06-12 15:35:57 -0500

    Version 1 - basic cell format.

--- whack.php
+++ whack.php
@@ -2,10 +2,25 @@
 <head>
 	<title>Whack'Em</title>
 	<script src="./js/jquery.js"></script>
-	<link rel="stylesheet" href="./css/bootstrap.css">
 	<style type="text/css">
 	.mole {
 		display: none;
+	}
+	
+	.cell {
+		height: 200px;
+		width: 200px;
+		border: solid black 1px;
+		float: left;
+	}
+	.row {
+		width: 606px;
+		height: 200px;
+	}
+	.main-container {
+		margin: auto;
+		width: 606px;
+		float: left;
 	}
 	</style>
 </head>
@@ -13,15 +28,53 @@
 
 
 
-<div class="container">
-	<img id="wtf" class="mole" src="./img/omar-cropped.png">
-</div>
+	<div class="main-container">
+
+		<div class="row">
+			<div id="1" class="cell">
+				<img class="mole" src="./img/cat-small.png">
+			</div>
+
+			<div id="2" class="cell">
+				<img class="mole" src="./img/cat-small.png">
+			</div>
+			<div id="3" class="cell">
+				<img class="mole" src="./img/cat-small.png">
+			</div>
+		</div>
+		
+		<div class="row">
+			<div id="4" class="cell">
+				<img class="mole" src="./img/cat-small.png">
+			</div>
+			<div id="5" class="cell">
+				<img class="mole" src="./img/cat-small.png">
+			</div>
+			<div id="6" class="cell">
+				<img class="mole" src="./img/cat-small.png">
+			</div>
+		</div>
+		
+		<div class="row">
+			<div id="7" class="cell">
+				<img class="mole" src="./img/cat-small.png">
+			</div>
+			<div id="8" class="cell">
+				<img class="mole" src="./img/cat-small.png">
+			</div>
+			<div id="9" class="cell">
+				<img class="mole" src="./img/cat-small.png">
+			</div>
+		</div>
+
+
+	</div>
 
 	<script type="text/javascript">
 
 	$('document').ready(function() {
 
-		$('#wtf').fadeIn();
+		$('.mole').fadeIn();
 
 		$('.mole').on('click', function() {
 			$(this).fadeOut();
