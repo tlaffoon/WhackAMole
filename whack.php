7adb268702417784f46cb953b0b500bec57ef8c1
Thomas J. Laffoon <tjl@thomasjlaffoon.com>
2014-06-14 01:10:43 -0500

    Working version; refactored functions heavily.  Some bugs remain.

--- whack.php
+++ whack.php
@@ -3,30 +3,51 @@
 	<title>Whack'Em</title>
 	<script src="./js/jquery.js"></script>
 	<style type="text/css">
+
+	body {
+		background-image: url('./img/cat-art.jpg');
+	}
 	.mole {
 		display: none;
-	}
-	
-	.cell {
-		height: 200px;
-		width: 200px;
-		border: solid black 1px;
-		float: left;
+		width: 198px;
+		height: 198px;
 	}
 	.row {
-		width: 606px;
+		width: 600px;
 		height: 200px;
 	}
+	.cell {
+		height: 198px;
+		width: 198px;
+		float: left;
+		border: solid black 1px;
+	}
 	.main-container {
-		margin: auto;
-		width: 606px;
+		margin-left: auto;
+		margin-right: auto;
+		width: 600px;
+		border: solid black 1px;
+	}
+	.bar {
+		width: 598px;
+	}
+	.start-button {
+		width: 200px;
 		float: left;
 	}
+	.score-box {
+		width: 398px;
+		text-align: right;
+		float: right;
+	}
+
+	.paw {
+		cursor: url(img/print.png), auto;
+	}
+
 	</style>
 </head>
 <body>
-
-
 
 	<div class="main-container">
 
@@ -58,26 +79,68 @@
 		<div class="row">
 			<div id="7" class="cell">
 				<img class="mole" src="./img/cat-small.png">
+
 			</div>
 			<div id="8" class="cell">
 				<img class="mole" src="./img/cat-small.png">
+
 			</div>
 			<div id="9" class="cell">
 				<img class="mole" src="./img/cat-small.png">
 			</div>
 		</div>
 
+		<div class="bar">
+			<button id="start" class="start-button">Start!</button>
+			<div id="score" class="score-box"></div>
+		</div>
 
-	</div>
+	</div>  <!-- end main container -->
 
 	<script type="text/javascript">
 
+	// 										//
+
+// function genActiveCell () {
+
+// };
+	
+function runGame () {
+	// doesn't clear interval?
+	clearInterval(game);
+
+	// points count double occasionally
+	var points = 0;
+	var time = 10;
+	var game = setInterval(function () {
+					if (time != 0) {
+						time--;
+						$('.mole').fadeOut();
+						var moles = $('.mole');
+						var activeCell = $(moles[Math.floor(Math.random()*moles.length)]);
+						$(activeCell).fadeIn();
+
+						activeCell.on('click', function() {
+							points += 10;
+							$('#score').text('Score: ' + points);
+							$('.mole').fadeOut();
+							console.log('Score: ' + points);
+						});					}
+
+					else {
+						console.log('game over. ' + points);	
+						clearInterval(game);
+					}
+				}, 2000);
+
+};
+	// 										//
+
 	$('document').ready(function() {
 
-		$('.mole').fadeIn();
-
-		$('.mole').on('click', function() {
-			$(this).fadeOut();
+		$('#start').on('click', function () { 
+			$('img').css('cursor', 'url(img/print.png), auto');
+			runGame();
 		});
 
 	});
